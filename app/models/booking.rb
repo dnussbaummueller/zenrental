27322d969dec07ce02ef8f272bad0921d50d5536
Daniel Nussbaummueller <d.nussbaummueller@gmail.com>
2023-02-27 17:10:20 +0100

    created the models and controllers and the seed file

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,14 @@
 class Booking < ApplicationRecord
   belongs_to :yoga_studio
   belongs_to :user
+
+  validates :booking_status, presence: true, inclusion: { in: %w[pending accepted declined] }
+  validates :checkin_date, presence: true
+  validates :checkout_date, presence: true
+  validates :yoga_studio, presence: true
+  validates :user, presence: true
+
+  def pending?
+    status == "pending"
+  end
 end
